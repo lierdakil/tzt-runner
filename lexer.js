a884c18a2e1165bfbea9bd39557fa2b072803d2e
Nikolay Yakimov <root@livid.pp.ru>
2023-08-05 03:29:54 +0300

    Arbitrary string escapes

--- lexer.js
+++ lexer.js
@@ -96,7 +96,7 @@ export function *lex(input, debug = false) {
     let buf = ""
     let tmp = ""
     while (curSt >= 0) {
-      if ([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,17,18,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102].includes(curSt)) {
+      if ([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,17,18,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102].includes(curSt)) {
         buf += tmp
         tmp = ""
         accSt = curSt
@@ -263,7 +263,7 @@ export function *lex(input, debug = false) {
         } else if (curCh === '\\') {
           curSt = 19
           continue
-        } else if (!(curCh === '"')) {
+        } else if (!(curCh === '\\' || curCh === '"')) {
           curSt = 16
           continue
         }
@@ -275,25 +275,7 @@ export function *lex(input, debug = false) {
         }
         break
       case 19:
-        if (curCh === '"') {
-          curSt = 21
-          continue
-        } else if (curCh === '\\') {
-          curSt = 19
-          continue
-        } else if (!(curCh === '"')) {
-          curSt = 16
-          continue
-        }
-        break
-      case 21:
-        if (curCh === '"') {
-          curSt = 18
-          continue
-        } else if (curCh === '\\') {
-          curSt = 19
-          continue
-        } else if (!(curCh === '"')) {
+        if (true) {
           curSt = 16
           continue
         }
@@ -1061,10 +1043,6 @@ export function *lex(input, debug = false) {
         yield [TokenType.Tok_word,  text ]
         continue
       case 18:
-        if (debug) console.log("Lexed token word: \"" + text + "\"")
-        yield [TokenType.Tok_word,  text ]
-        continue
-      case 21:
         if (debug) console.log("Lexed token word: \"" + text + "\"")
         yield [TokenType.Tok_word,  text ]
         continue
